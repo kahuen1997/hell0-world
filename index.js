14bc21a2bfd6b71c303483711dcfdd98146e093e
edmond-test <kahuen1997.career@gmail.com>
2024-11-04 17:04:37 +0800

    oh canada

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const x = document.getElementById('en');
 const y = document.getElementById('re');
-const z = document.getElementById('nn')
+const z = document.getElementById('nn');
+const f = document.getElementById('ene');
 
 z.addEventListener("click", () => {
     x.focus();
@@ -11,3 +12,15 @@ z.addEventListener("click", () => {
         y.innerHTML = ""; // 清空错误信息，当输入有效时
     }
 });
+
+f.addEventListener("input", () => {
+    f.focus();
+    
+    if (!f.checkValidity()) {
+        y.innerHTML = f.validationMessage;
+    } else {
+        y.innerHTML = ""; // 清空错误信息，当输入有效时
+    }
+});
+
+
