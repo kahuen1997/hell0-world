f84f576bad1d3805c6f845f7d7b417a95cf751f8
edmond-test <kahuen1997.career@gmail.com>
2024-11-06 15:26:05 +0800

    fixed the checking in email input-group

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-const x = document.getElementById('en');
-const y = document.getElementById('re');
-const z = document.getElementById('nn');
-const f = document.getElementById('ene');
+const emailinput = document.getElementById('mosemailinput')
+const emailbutton = document.getElementById('mosemailbutton')
+const emailwrongmsg = document.getElementById('emailmsg')
 
-z.addEventListener("click", () => {
-    x.focus();
-    
-    if (!x.checkValidity()) {
-        y.innerHTML = x.validationMessage;
-    } else {
-        y.innerHTML = ""; // 清空错误信息，当输入有效时
+emailbutton.addEventListener("click", ()=>{
+    if (emailinput.value===null||emailinput.value===""){
+        emailwrongmsg.style.color = 'red'
+        emailwrongmsg.innerHTML = "Please input email";
+    } else if (!emailinput.checkValidity()){
+        emailwrongmsg.style.color = 'red'
+        emailwrongmsg.innerHTML = emailinput.validationMessage;
+    } else{
+        emailwrongmsg.innerHTML = "";
     }
-});
-
-f.addEventListener("input", () => {
-    f.focus();
-    
-    if (!f.checkValidity()) {
-        y.innerHTML = f.validationMessage;
-    } else {
-        y.innerHTML = ""; // 清空错误信息，当输入有效时
-    }
-});
-
-
+})
